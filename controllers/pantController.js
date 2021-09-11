dd92fe100a7c192e134429bb58c79a493d75b40a
antelor <ramiro.antelo4@gmail.com>
2021-09-11 16:13:11 -0300

    pant create post implementado. error en links de navbar arreglado

--- controllers/pantController.js
+++ controllers/pantController.js
@@ -1,5 +1,8 @@
 var Pant = require('../models/pant');
+var Brand = require('../models/brand');
+var Size = require('../models/size');
 var async = require('async');
+const { body, validationResult } = require('express-validator');
 
 // Display list of all pants.
 exports.pant_list = function(req, res) {
@@ -39,14 +42,92 @@ exports.pant_detail = function(req, res, next) {
 };
 
 // Display pant create form on GET.
-exports.pant_create_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: pant create GET');
+exports.pant_create_get = function (req, res, next) {
+    async.parallel({
+        brands: function (callback) {
+            Brand.find(callback);
+        },
+        sizes: function (callback) {
+            Size.find(callback);
+        },
+    }, function (err, results) {
+        if (err) { return next(err); }
+        res.render('pant_form', { title: 'Create Pant', errors: err, pant:null, brands:results.brands, sizes:results.sizes });
+    })
 };
 
 // Handle pant create on POST.
-exports.pant_create_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: pant create POST');
-};
+exports.pant_create_post = [
+    (req, res, next) => {
+
+        // Convert the brand to an array.
+        if(!(req.body.brand instanceof Array)){
+            if(typeof req.body.brand ==='undefined')
+            req.body.brand = [];
+            else
+            req.body.brand = new Array(req.body.brand);
+        }
+
+        // Convert the size to an array.
+        if(!(req.body.size instanceof Array)){
+            if(typeof req.body.size ==='undefined')
+            req.body.size = [];
+            else
+            req.body.size = new Array(req.body.size);
+        }
+        next();
+    },
+    
+    // Validate and sanitise fields.
+    body('name', 'Name must not be empty.').trim().isLength({ min: 1 }).escape(),
+    body('desc', 'Description must not be empty.').trim().isLength({ min: 1 }).escape(),
+    body('stock').isNumeric().withMessage('Only numbers allowed in stock field'),
+    body('brand.*').escape(),
+    body('size.*').escape(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a pant object with escaped and trimmed data.
+        var pant = new Pant(
+          { name: req.body.name,
+            desc: req.body.desc,
+            brand: req.body.brand,
+            stock: parseInt(req.body.stock),
+            size: req.body.size
+           });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/error messages.
+
+            // Get all authors and genres for form.
+            async.parallel({
+                brands: function (callback) {
+                    Brand.find(callback);
+                },
+                sizes: function (callback) {
+                    Size.find(callback);
+                },
+            }, function(err, results) {
+                if (err) { return next(err); }
+
+                res.render('pant_form', { title: 'Create pant',authors:results.authors, brands:results.brands, sizes:results.sizes, pant: pant, errors: errors.array() });
+            });
+            return;
+        }
+        else {
+            // Data from form is valid. Save pant.
+            pant.save(function (err) {
+                if (err) { return next(err); }
+                   //successful - redirect to new pant record.
+                   res.redirect(pant.url);
+                });
+        }
+    }
+];
 
 // Display pant delete form on GET.
 exports.pant_delete_get = function(req, res) {
