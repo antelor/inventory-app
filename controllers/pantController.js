ef9d8e96dae176feb689a2d34ef8aeb5183d35bd
antelor <ramiro.antelo4@gmail.com>
2021-09-14 16:29:38 -0300

    pant update implementado

--- controllers/pantController.js
+++ controllers/pantController.js
@@ -163,11 +163,128 @@ exports.pant_delete_post = function (req, res) {
 };
 
 // Display pant update form on GET.
-exports.pant_update_get = function(req, res) {
-    res.send('NOT IMPLEMENTED: pant update GET');
+exports.pant_update_get = function(req, res, next) {
+
+    // Get pant, brands and sizes for form.
+    async.parallel({
+        pant: function(callback) {
+            Pant.findById(req.params.id).populate('brand').populate('size').exec(callback);
+        },
+        brands: function(callback) {
+            Brand.find(callback);
+        },
+        sizes: function(callback) {
+            Size.find(callback);
+        },
+        }, function(err, results) {
+            if (err) { return next(err); }
+            if (results.pant==null) { // No results.
+                var err = new Error('pant not found');
+                err.status = 404;
+                return next(err);
+            }
+            // Success.
+        
+            // Mark our selected brands as checked.
+            for (var all_b_iter = 0; all_b_iter < results.brands.length; all_b_iter++) {
+                for (var pant_b_iter = 0; pant_b_iter < results.pant.brand.length; pant_b_iter++) {
+                    if (results.brands[all_b_iter]._id.toString()===results.pant.brand[pant_b_iter]._id.toString()) {
+                        results.brands[all_b_iter].checked = true;
+                    }
+                }
+            }
+
+            // Mark our selected size as checked.
+            for (var all_s_iter = 0; all_s_iter < results.sizes.length; all_s_iter++) {
+                    if (results.sizes[all_s_iter]._id.toString()===results.pant.size._id.toString()) {
+                        results.sizes[all_s_iter].checked = true;
+                    }
+            };
+        
+            res.render('pant_form', { title: 'Update pant', brands: results.brands, sizes: results.sizes, pant: results.pant, errors: err });
+        });
+
 };
 
 // Handle pant update on POST.
-exports.pant_update_post = function(req, res) {
-    res.send('NOT IMPLEMENTED: pant update POST');
-};
+exports.pant_update_post = [
+
+    // Convert the genre to an array
+    (req, res, next) => {
+        if(!(req.body.brand instanceof Array)){
+            if(typeof req.body.brand==='undefined')
+            req.body.brand=[];
+            else
+            req.body.brand=new Array(req.body.brand);
+        }
+        next();
+    },
+
+    // Validate and sanitise fields.
+    body('name', 'Name must not be empty.').trim().isLength({ min: 1 }).escape(),
+    body('desc', 'Description must not be empty.').trim().isLength({ min: 1 }).escape(),
+    body('stock').isNumeric().withMessage('Only numbers allowed in stock field'),
+    body('brand.*').escape(),
+    body('size.*').escape(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+
+        // Create a pant object with escaped/trimmed data and old id.
+        var pant = new Pant(
+          { name: req.body.desc,
+            desc: req.body.desc,
+            stock: req.body.stock,
+            brand: (typeof req.body.brand === 'undefined') ? [] : req.body.brand,
+            size: req.body.size,
+            _id:req.params.id //This is required, or a new ID will be assigned!
+           });
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/error messages.
+
+            // Get all authors and genres for form.
+            async.parallel({
+                brands: function(callback) {
+                    Brand.find(callback);
+                },
+                sizes: function(callback) {
+                    Size.find(callback);
+                },
+            }, function(err, results) {
+                if (err) { return next(err); }
+
+                
+                // Mark our selected brands as checked.
+                for (var all_b_iter = 0; all_b_iter < results.brands.length; all_b_iter++) {
+                    for (var pant_b_iter = 0; pant_b_iter < results.pant.brand.length; pant_b_iter++) {
+                        if (results.brands[all_b_iter]._id.toString()===results.pant.brand[pant_b_iter]._id.toString()) {
+                            results.brands[all_b_iter].checked = true;
+                        }
+                    }
+                }
+
+                // Mark our selected size as checked.
+                for (var all_s_iter = 0; all_s_iter < results.sizes.length; all_s_iter++) {
+                        if (results.sizes[all_s_iter]._id.toString()===results.pant.size._id.toString()) {
+                            results.sizes[all_s_iter].checked = true;
+                        }
+                };
+
+                res.render('pant_form', { title: 'Update pant', brands: results.brands, sizes: results.sizes, pant: pant, errors: errors.array() });
+            });
+            return;
+        }
+        else {
+            // Data from form is valid. Update the record.
+            Pant.findByIdAndUpdate(req.params.id, pant, {}, function (err,thepant) {
+                if (err) { return next(err); }
+                   // Successful - redirect to pant detail page.
+                   res.redirect(thepant.url);
+                });
+        }
+    }
+];
